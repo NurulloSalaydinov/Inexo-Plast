0749dd5dd8ab687b1cc411f488dff7affc2ccc1a
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-08-22 17:17:29 +0300

    static collected

--- DjangoBackend/inexoplast/inexoplast/settings.py
+++ DjangoBackend/inexoplast/inexoplast/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wz^!%-3=4wklc$mnnow$$oyg_9i(n7+$(-(9$--*1na@)x2n9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- DjangoBackend/inexoplast/inexoplast/urls.py
+++ DjangoBackend/inexoplast/inexoplast/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
+
+# from django.views.static import serve
 
 from main import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
