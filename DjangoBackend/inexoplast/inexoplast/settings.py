87cb9c2111b17ac87b145aeae3c846611122ab5a
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-08-23 10:39:23 +0300

    settings updated

--- DjangoBackend/inexoplast/inexoplast/settings.py
+++ DjangoBackend/inexoplast/inexoplast/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wz^!%-3=4wklc$mnnow$$oyg_9i(n7+$(-(9$--*1na@)x2n9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
